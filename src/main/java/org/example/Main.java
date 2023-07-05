8b67934903fb0427e6d634a4cbe944f427ef903f
GoogleChromeIsGarbo <mateo.hincapie.florez@gmail.com>
2023-07-05 11:55:23 -0500

    first example modify class on Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.example.modelos.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Integer edad = 19;
+       // LOS OBJETOS SON VARIABLES, ESPECIALES
+
+        Universidad objetoUniversidad = new Universidad ("Camilo");
+
+        Usuario objetoUsuario = new Usuario(21,"Camilo","123",4,"camilorozco@gmail.com");
+
+        //creo un objeto para acceder o modificar los atributos o metodos de ese objeto o elemento
+
+        System.out.println(objetoUniversidad.nombres);
+        System.out.println(objetoUsuario);
     }
 }
---
+++ src/main/java/org/example/Universidad.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Universidad {
+
+  public String nombres;
+
+    public Universidad() {
+    }
+
+    public Universidad(String nombres) {
+        this.nombres = nombres;
+    }
+}
